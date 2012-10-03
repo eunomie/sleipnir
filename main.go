16c5131c944a2892022522409649e60f24438acd
CrEv <yves.brissaud@gmail.com>
2012-10-03 23:03:43 +0200

    Add: read parameters from the command line

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"github.com/elazarl/goproxy"
 	"io/ioutil"
@@ -50,7 +51,7 @@ func CreateDefault(listen string, configFile string, verbose bool) {
 	proxy.Verbose = verbose
 
 	records := getConfig(configFile)
-	fmt.Println("Configuration:")
+	fmt.Println("Configuration from", configFile, ":")
 	for i, config := range records {
 		if i == 0 {
 			// header line
@@ -68,6 +69,7 @@ func CreateDefault(listen string, configFile string, verbose bool) {
 			})
 	}
 	fmt.Println("")
+	fmt.Println("Listen", listen)
 	fmt.Println("^C to stop")
 	fmt.Println("")
 
@@ -75,5 +77,20 @@ func CreateDefault(listen string, configFile string, verbose bool) {
 }
 
 func main() {
-	CreateDefault(":8888", "config.csv", false)
+	var addr string
+	var configFile string
+	var verbose bool
+	var help bool
+	flag.StringVar(&addr, "a", ":8888", "Bind to this address:port")
+	flag.StringVar(&configFile, "c", "config.csv", "Config file")
+	flag.BoolVar(&verbose, "v", false, "Verbose")
+	flag.BoolVar(&help, "h", false, "Print this help")
+
+	flag.Parse()
+	if help {
+		flag.Usage()
+		return
+	}
+
+	CreateDefault(addr, configFile, verbose)
 }
