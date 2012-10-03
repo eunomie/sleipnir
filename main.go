f04c15e92559420372b5ce26a566b69d17fae767
CrEv <yves.brissaud@gmail.com>
2012-10-03 07:29:28 +0200

    default style from sublime

--- main.go
+++ main.go
@@ -1,76 +1,76 @@
 package main
 
 import (
-  "encoding/csv"
-  "fmt"
-  "github.com/elazarl/goproxy"
-  "log"
-  "net/http"
-  "os"
+	"encoding/csv"
+	"fmt"
+	"github.com/elazarl/goproxy"
+	"log"
+	"net/http"
+	"os"
 )
 
 func getConfig(configFile string) [][]string {
-  file, e := os.Open(configFile)
-  if e != nil {
-    panic(e)
-  }
-  defer file.Close()
+	file, e := os.Open(configFile)
+	if e != nil {
+		panic(e)
+	}
+	defer file.Close()
 
-  csvReader := csv.NewReader(file)
-  records, e := csvReader.ReadAll()
-  if e != nil {
-    panic(e)
-  }
+	csvReader := csv.NewReader(file)
+	records, e := csvReader.ReadAll()
+	if e != nil {
+		panic(e)
+	}
 
-  return records
+	return records
 }
 
 func NewResponse(r *http.Request, contentType string, status int, contentFileName string) *http.Response {
-  resp := &http.Response{}
-  resp.Request = r
-  resp.TransferEncoding = r.TransferEncoding
-  resp.Header = make(http.Header)
-  resp.Header.Add("Content-Type", contentType)
-  resp.StatusCode = status
-  file, e := os.Open(contentFileName)
-  if e != nil {
-    panic(e)
-  }
-  defer file.Close()
-  fileInfo, e := file.Stat()
-  if e != nil {
-    panic(e)
-  }
-  resp.ContentLength = fileInfo.Size()
-  resp.Body = file
-  return resp
+	resp := &http.Response{}
+	resp.Request = r
+	resp.TransferEncoding = r.TransferEncoding
+	resp.Header = make(http.Header)
+	resp.Header.Add("Content-Type", contentType)
+	resp.StatusCode = status
+	file, e := os.Open(contentFileName)
+	if e != nil {
+		panic(e)
+	}
+	defer file.Close()
+	fileInfo, e := file.Stat()
+	if e != nil {
+		panic(e)
+	}
+	resp.ContentLength = fileInfo.Size()
+	resp.Body = file
+	return resp
 }
 
 func CreateDefault(listen string, configFile string, verbose bool) {
-  proxy := goproxy.NewProxyHttpServer()
-  proxy.Verbose = verbose
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = verbose
 
-  records := getConfig(configFile)
-  fmt.Println("Configuration:")
-  for i, config := range records {
-    if i == 0 {
-      // header line
-      continue
-    }
-    fmt.Println("- replace", config[0], "by", config[2])
-    proxy.OnRequest(goproxy.UrlIs(config[0])).DoFunc(
-      func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-        fmt.Println("Intercept", config[0])
-        return r, NewResponse(r, config[1], http.StatusOK, config[2])
-      })
-  }
-  fmt.Println("")
-  fmt.Println("^C to stop")
-  fmt.Println("")
+	records := getConfig(configFile)
+	fmt.Println("Configuration:")
+	for i, config := range records {
+		if i == 0 {
+			// header line
+			continue
+		}
+		fmt.Println("- replace", config[0], "by", config[2])
+		proxy.OnRequest(goproxy.UrlIs(config[0])).DoFunc(
+			func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+				fmt.Println("Intercept", config[0])
+				return r, NewResponse(r, config[1], http.StatusOK, config[2])
+			})
+	}
+	fmt.Println("")
+	fmt.Println("^C to stop")
+	fmt.Println("")
 
-  log.Fatal(http.ListenAndServe(listen, proxy))
+	log.Fatal(http.ListenAndServe(listen, proxy))
 }
 
 func main() {
-  CreateDefault(":8888", "config.csv", false)
+	CreateDefault(":8888", "config.csv", false)
 }
