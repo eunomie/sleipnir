497a11fd43a2ce52674b98d0d83372dbf3e46830
CrEv <yves.brissaud@gmail.com>
2012-10-03 23:59:10 +0200

    Add: quick and dirty cache

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ import (
 	"os"
 )
 
+var contents = map[string][]byte{}
+var dates = map[string]int64{}
+
 func getConfig(configFile string) [][]string {
 	file, e := os.Open(configFile)
 	if e != nil {
@@ -28,6 +31,20 @@ func getConfig(configFile string) [][]string {
 	return records
 }
 
+func getFileModDate(fileName string) (int64, bool) {
+	file, e := os.Open(fileName)
+	if e != nil {
+		fmt.Errorf("Error when reading", fileName, e.Error())
+		return 0, false
+	}
+	fi, e := file.Stat()
+	if e != nil {
+		fmt.Errorf("Error when reading informations from", fileName, e.Error())
+		return 0, false
+	}
+	return fi.ModTime().Unix(), true
+}
+
 func NewResponse(r *http.Request, contentType string, status int, contentFileName string) *http.Response {
 	resp := &http.Response{}
 	resp.Request = r
@@ -36,11 +53,32 @@ func NewResponse(r *http.Request, contentType string, status int, contentFileNam
 	resp.Header.Add("Content-Type", contentType)
 	resp.StatusCode = status
 
-	b, e := ioutil.ReadFile(contentFileName)
-	if e != nil {
-		fmt.Errorf("Error when reading", contentFileName, e.Error())
+	var b []byte
+
+	currentDate, ok := getFileModDate(contentFileName)
+	if !ok {
 		return nil
 	}
+	date, ok := dates[contentFileName]
+	if !ok {
+		date = 0
+	}
+
+	var e error
+	if currentDate > date {
+		b, e = ioutil.ReadFile(contentFileName)
+		if e != nil {
+			fmt.Errorf("Error when reading", contentFileName, e.Error())
+			return nil
+		}
+		contents[contentFileName] = b
+		dates[contentFileName] = currentDate
+		fmt.Println("")
+	} else {
+		fmt.Println(" [from cache]")
+		b, _ = contents[contentFileName]
+	}
+
 	reader := bytes.NewReader(b)
 	resp.ContentLength = int64(reader.Len())
 	resp.Body = ioutil.NopCloser(reader)
@@ -64,7 +102,7 @@ func CreateDefault(listen string, configFile string, verbose bool) {
 		fmt.Println("- replace", defaultUrl, "by", newFile)
 		proxy.OnRequest(goproxy.UrlIs(defaultUrl)).DoFunc(
 			func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-				fmt.Println("Intercept", defaultUrl)
+				fmt.Print("Intercept ", defaultUrl, " and serve ", newFile)
 				response := NewResponse(r, contentType, http.StatusOK, newFile)
 				return r, response
 			})
